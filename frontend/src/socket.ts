89a924181cc3d850b44737b3bc39c5e1607760dc
dariogguillen <guillendario@gmail.com>
2024-10-14 14:58:00 -0600

    fix: remove port

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client"; // import connection function
 import { backendUrl } from "./utils/config.default";
 
-const socket = socketio.connect(backendUrl, { path: "/", port: "80" }); // initialize websocket connection
+const socket = socketio.connect(backendUrl, { path: "/" }); // initialize websocket connection
 
 export default socket;
