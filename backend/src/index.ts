21fb170465205f9e699f656deec876b97560b3c7
dariogguillen <guillendario@gmail.com>
2024-10-14 00:11:10 -0600

    feat: socket events

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,153 @@
 import express from "express";
-import cors from "cors";
+import { v4 as uuidV4 } from "uuid";
+import http from "http";
+import { Server } from "socket.io";
 
-const app = express();
-app.use(express.json()); // for parsing application/json responses
+try {
+  const app = express();
 
-if (process.env.FRONTEND_URL) {
-  // If provided,only allow connections from the frontend website to be
-  // specified in an environment variable file
-  const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-  };
-  app.use(cors(corsOptions));
-} else {
-  // if a server is not specified in environment variables,
-  // allow connections from all origins
-  app.use(cors());
+  console.log({ app });
+
+  const server = http.createServer(app);
+
+  console.log({ server });
+
+  // set port to value received from environment variable or 8080 if null
+  const port = process.env.PORT || 8080;
+
+  console.log({ port });
+
+  // upgrade http server to websocket server
+  const io = new Server(server, {
+    cors: process.env.FRONTEND_URL
+      ? { origin: process.env.FRONTEND_URL }
+      : { origin: "*" }, // allow connection from any origin
+  });
+
+  // TODO: Use a proper DB
+  const rooms = new Map();
+
+  // io.connection;
+  io.on("connection", (socket) => {
+    // socket refers to the client socket that just got connected.
+    // each socket is assigned an id
+    console.log(socket.id, "connected");
+
+    socket.on("username", (username) => {
+      console.log("username:", username);
+      socket.data.username = username;
+    });
+
+    // createRoom
+    socket.on("createRoom", async (callback) => {
+      // callback here refers to the callback function from the client passed as data
+      const roomId = uuidV4();
+      await socket.join(roomId);
+
+      // set roomId as a key and roomData including players as value in the map
+      rooms.set(roomId, {
+        roomId,
+        players: [{ id: socket.id, username: socket.data?.username }],
+      });
+      // returns Map(1){'2b5b51a9-707b-42d6-9da8-dc19f863c0d0' => [{id: 'socketid', username: 'username1'}]}
+
+      callback(roomId);
+    });
+
+    socket.on("joinRoom", async (args, callback) => {
+      // check if room exists and has a player waiting
+      const room = rooms.get(args.roomId);
+      let error, message;
+
+      if (!room) {
+        // if room does not exist
+        error = true;
+        message = "room does not exist";
+      } else if (room.length <= 0) {
+        // if room is empty set appropriate message
+        error = true;
+        message = "room is empty";
+      } else if (room.length >= 2) {
+        // if room is full
+        error = true;
+        message = "room is full"; // set message to 'room is full'
+      }
+
+      if (error) {
+        // if there's an error, check if the client passed a callback,
+        // call the callback (if it exists) with an error object and exit or
+        // just exit if the callback is not given
+
+        if (callback) {
+          // if user passed a callback, call it with an error payload
+          callback({
+            error,
+            message,
+          });
+        }
+
+        return; // exit
+      }
+
+      await socket.join(args.roomId); // make the joining client join the room
+
+      // add the joining user's data to the list of players in the room
+      const roomUpdate = {
+        ...room,
+        players: [
+          ...room.players,
+          { id: socket.id, username: socket.data?.username },
+        ],
+      };
+
+      rooms.set(args.roomId, roomUpdate);
+
+      callback(roomUpdate); // respond to the client with the room details.
+
+      // emit an 'opponentJoined' event to the room to tell the other player that an opponent has joined
+      socket.to(args.roomId).emit("opponentJoined", roomUpdate);
+    });
+
+    socket.on("move", (data) => {
+      // emit to all sockets in the room except the emitting socket.
+      socket.to(data.room).emit("move", data.move);
+    });
+
+    socket.on("disconnect", () => {
+      const gameRooms = Array.from(rooms.values());
+      gameRooms.forEach((room) => {
+        const userInRoom = room.players.find(
+          (player) => player.id === socket.id,
+        );
+
+        if (userInRoom) {
+          if (room.players.length < 2) {
+            // if there's only 1 player in the room, close it and exit.
+            rooms.delete(room.roomId);
+            return;
+          }
+
+          socket.to(room.roomId).emit("playerDisconnected", userInRoom); // <- 4
+        }
+      });
+    });
+    socket.on("closeRoom", async (data) => {
+      socket.to(data.roomId).emit("closeRoom", data); // <- 1 inform others in the room that the room is closing
+
+      const clientSockets = await io.in(data.roomId).fetchSockets(); // <- 2 get all sockets in a room
+
+      // loop over each socket client
+      clientSockets.forEach((s) => {
+        s.leave(data.roomId); // <- 3 and make them leave the room on socket.io
+      });
+
+      rooms.delete(data.roomId); // <- 4 delete room from rooms map
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+  });
+} catch (err) {
+  console.error(err);
 }
-
-app.get("/api/helloworld", (req, res) => {
-  res.send("Hello World! From the backend!");
-});
-
-app.get("/api/wake-up", (req, res) => {
-  res.send("I am awake!");
-});
-
-const { PORT = 3000 } = process.env;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
